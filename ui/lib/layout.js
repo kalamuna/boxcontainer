e868bd3bbd24879d1ac1fb92b2acaf0aa6138b99
James Halliday <mail@substack.net>
2014-03-27 18:31:09 -0700

    fixed the container layout

--- ui/lib/layout.js
+++ ui/lib/layout.js
@@ -1,8 +1,15 @@
 var hyperstream = require('hyperstream');
 var duplexer = require('duplexer2');
 var through = require('through2');
+var fs = require('fs');
 
 module.exports = function () {
     var input = through();
-    return duplexer(input, hyperstream({ '#content': input }));
+    var hs = hyperstream({ '#content': input });
+    readStream('layout.html').pipe(hs);
+    return duplexer(input, hs);
 };
+
+function readStream (file) {
+    return fs.createReadStream(__dirname + '/../static/' + file);
+}
