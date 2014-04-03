09fcb80d2ac6235aae158d9b07d46932a4a58f10
James Halliday <mail@substack.net>
2014-04-03 09:22:54 -0700

    friendly sizes

--- ui/render/image.js
+++ ui/render/image.js
@@ -1,7 +1,15 @@
+var humanize = require('human-format');
+
 module.exports = function () {
     return function (row) {
         return {
-            // ...
+            '.id': row.id.slice(0,6),
+            '.parent': row.parentId.slice(0,6),
+            '.tags': (row.repoTags || []).map(function (t) {
+                return { '.tag': t };
+            }),
+            '.disk-size': humanize(row.size),
+            '.virtual-size': humanize(row.virtualSize)
         };
     };
 };
