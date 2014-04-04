320b103a7d85c5d36646c77b40f42109a9d94503
James Halliday <mail@substack.net>
2014-04-03 19:14:06 -0700

    color-coded ids

--- ui/render/image.js
+++ ui/render/image.js
@@ -2,9 +2,13 @@ var humanize = require('human-format');
 
 module.exports = function () {
     return function (row) {
+        var id = row.id.slice(0,6);
+        var pid = row.parentId.slice(0,6);
         return {
-            '.id': row.id.slice(0,6),
-            '.parent': row.parentId.slice(0,6),
+            '.id': id,
+            '.id-color': { style: 'background-color: #' + id },
+            '.parent': pid,
+            '.parent-color': { style: 'background-color: #' + pid },
             '.fork': row.parentId ? {} : { style: 'display: none' },
             '.tag': row.repoTags || [],
             '.disk-size': humanize(row.size),
